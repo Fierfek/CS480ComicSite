e03e72c2bebb298ada3ba710720071724caa0074
Unknown <marisabelchang@gmail.com>
2018-04-30 10:14:58 -0700

    update the navBar.html and navBaCtr

--- public/js/controllers/NavBarCtrl.js
+++ public/js/controllers/NavBarCtrl.js
@@ -2,4 +2,9 @@ var navBar = angular.module('NavBarCtrl', []);
 
 navBar.controller('NavBarController', function($scope) {
 	$scope.searchTest = 'Advanced Search';
+	$scope.data= ["Book tittle","Writer","Illustrator","Character","Volume","Issue","Year"];
+	$scope.search="search";
+	$scope.getSearchInfo=function(info){
+		$scope.search=document.getElementById(info).value;
+	}
 });
