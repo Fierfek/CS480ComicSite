1a7d7c1a7218b310d01dc8c3e918664d08ae0772
Unknown <marisabelchang@gmail.com>
2018-04-30 11:33:47 -0700

    delete the header and body to the navBar

--- public/js/controllers/NavBarCtrl.js
+++ public/js/controllers/NavBarCtrl.js
@@ -3,8 +3,9 @@ var navBar = angular.module('NavBarCtrl', []);
 navBar.controller('NavBarController', function($scope) {
 	$scope.searchTest = 'Advanced Search';
 	$scope.data= ["Book tittle","Writer","Illustrator","Character","Volume","Issue","Year"];
-	$scope.search="search";
-	$scope.getSearchInfo=function(info){
-		$scope.search=document.getElementById(info).value;
+
+	$scope.startSearch = function getSearch(search){
+		var searchParams = search.searchData;
+		console.log("Search for: " + searchParams);
 	}
 });
