1b103ccaea795d8331f45316eb7912d22b37e6a4
Sean <s.ritchie577@gmail.com>
2018-05-03 16:39:05 -0700

    Account Creation Page
    Set up the html and basic angular for creating an account, as well as fixing links in this page and fixing links in the login page

---
+++ public/js/controllers/SignupCtrl.js
@@ -0,0 +1,38 @@
+var app = angular.module('SignupCtrl', []);
+
+app.controller('SignupController', function($scope, $rootScope, $stateParams, $state, SignupService){
+    
+    $scope.formSubmit = function() {
+      if(SignupService.Signup($scope.username, $scope.password, $scope.passwordConfirm, $scope.email)) {
+        $scope.error = '';
+        $scope.username = '';
+        $scope.password = '';
+        $scope.passwordConfirm = '';
+        $scope.email = '';
+        $state.transitionTo('home');
+      } else {
+        $scope.error = "Invalid entry!";
+      }   
+    };
+
+
+
+    app.factory('LoginService', function() {
+    var admin = 'admin';
+    var pass = 'pass';
+    var confirmPass = 'confirmPass';
+    var mail = 'mail';
+    var isAuthenticated = false;
+    
+    return {
+      login : function(username, password, passwordConfirm, email) {
+        isAuthenticated = username === admin && password === pass && passwordConfirm === confirmPass && email = mail;
+        return isAuthenticated;
+      },
+      isAuthenticated : function() {
+        return isAuthenticated;
+      }
+    };
+    
+});
+});
