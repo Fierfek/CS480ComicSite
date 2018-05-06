e368f9a592f8fd437a43d238aad3947a2733acfc
Unknown <marisabelchang@gmail.com>
2018-05-06 00:36:40 -0700

    Create services and directives files. Update some files

--- public/js/controllers/SignupCtrl.js
+++ public/js/controllers/SignupCtrl.js
@@ -1,42 +1,23 @@
-var app = angular.module('SignupCtrl', []);
+var signUpPage = angular.module('SignupCtrl', []);
 
-app.controller('SignupController', function($scope, $rootScope, $stateParams, $state, SignupService){
-    
-    $scope.formSubmit = function() {
-      if(SignupService.Signup($scope.username, $scope.password, $scope.passwordConfirm, $scope.email)) {
-        $scope.error = '';
-        $scope.username = '';
-        $scope.password = '';
-        $scope.passwordConfirm = '';
-        $scope.email = '';
-        $scope.question1 = '';
-        $scope.answer1 = '';
-        $scope.question2 = '';
-        $scope.answer2 = '';
-        $state.transitionTo('home');
-      } else {
-        $scope.error = "Invalid entry!";
-      }   
+signUpPage.controller('SignupController', function ($scope, $rootScope, $routeParams, $location, $http, RestApiClientService) {
+ 
+    $scope.signup = {};
+ 
+    $scope.signup = {username:'',password:'',email:'',question1:'',answer1:'',question2:'',answer2:''};
+ 
+    $scope.signUp = function (customer) {
+        RestApiClientService.post('signup', {
+            customer: customer
+        }).then(function (results) {
+            RestApiClientService.toast(results);
+            if (results.status == "success") {
+                $location.path('profile');
+            }
+        });
     };
-
-
-
-    app.factory('LoginService', function() {
-    var admin = 'admin';
-    var pass = 'pass';
-    var confirmPass = 'confirmPass';
-    var mail = 'mail';
-    var isAuthenticated = false;
-    
-    return {
-      login : function(username, password, passwordConfirm, email) {
-        isAuthenticated = username === admin && password === pass && passwordConfirm === confirmPass && email = mail;
-        return isAuthenticated;
-      },
-      isAuthenticated : function() {
-        return isAuthenticated;
-      }
-    };
-    
+ 
 });
-});
+
+
+
