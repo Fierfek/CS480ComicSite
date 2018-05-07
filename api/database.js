28ed084dcf0a9fe4e0781e0b0cdb62a4f00bb394
Brennan Zuber <geekssw@yahoo.com>
2018-05-07 13:52:18 -0700

    Write signed in query

--- api/database.js
+++ api/database.js
@@ -3,6 +3,12 @@ var router = express.Router();
 
 router.use('/book', require(__dirname + '/bookCrud.js'));
 
+router.ask('/signedIn', function(req, res) {
+	console.log(req);
+	var signedIn = true;
+	res.send(signedIn);
+});
+
 router.get('/', function(req, res) {
 	console.log("get anything");
 });
