f6aca44136c6eeb3804c69c29b0b51d7bbdd9e00
Brennan Zuber <geekssw@yahoo.com>
2018-05-07 09:31:45 -0700

    First Crud changes

---
+++ api/bookCrud.js
@@ -0,0 +1,72 @@
+var express = require('express');
+var router = express.Router();
+var aws = require("aws-sdk");
+
+aws.config.update({
+	accessKeyId: "AKIAJAX7OK37HX73JBLA",
+	secretAccessKey: "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",
+	region: "us-west-2",
+	//endpoint: "http://localhost:8000"
+});
+
+var docClient = new aws.DynamoDB.DocumentClient();
+
+router.post('/create', function(req, res) {
+	var params = {
+		TableName: "Book",
+		Item: {
+			"bookID": req.bookID,
+			"title": req.title,
+			"issuesList": req.issuesList,
+		}
+	}
+	
+	docClient.put(params, function(err, data) {
+		if(err) {
+			console.log("Unable to add item. Error JSON: " + JSON.stringify(err, null, 2));
+		} else {
+			console.log("Added Item:" + JSON.stringify(data, null, 2));
+		}
+	});
+});
+
+router.post('/create', function(req, res) {
+	var params = {
+		TableName: "Book",
+		Item: {
+			"bookID": req.bookID,
+			"title": req.title,
+			"issuesList": req.issuesList,
+		}
+	}
+	
+	docClient.put(params, function(err, data) {
+		if(err) {
+			console.log("Unable to add item. Error JSON: " + JSON.stringify(err, null, 2));
+		} else {
+			console.log("Added Item:" + JSON.stringify(data, null, 2));
+		}
+	});
+});
+
+router.get('/read', function(req, res) {
+	var params = {
+		TableName: "Book",
+		ProjectionExpression: "bookID, title, issueList"
+	}
+	
+	docClient.scan(params, function(err, data) {
+		if(err) {
+			console.log("Unable to scan the table. Error JSON: " + JSON.stringify(err, null, 2));
+		} else {
+			console.log("Scan success");
+			res.send(data.Items);
+			data.Items.forEach(function(book) {
+				console.log(book.bookID + " | " + book.title);
+			});
+		}
+	});
+	
+});
+
+module.exports = router;
