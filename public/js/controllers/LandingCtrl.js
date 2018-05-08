613136760a21c3b45545abea4cf1ecd4f08fcd58
Sean <s.ritchie577@gmail.com>
2018-05-08 16:51:35 -0700

    Basic Framework
    Waiting on back end changes

--- public/js/controllers/LandingCtrl.js
+++ public/js/controllers/LandingCtrl.js
@@ -4,5 +4,13 @@ landingPage.controller('LandingPage', function($scope, RestApiClientService) {
 	$scope.tagline = 'To the moon and back!';
 
 	
-	
+	RestApiClientService.get('/book').then(function(response) {
+		
+		$scope.books = response;
+		
+		for (var i = 0; i < $scope.books.length; i++) {
+			var list = $scope.books[i].issueList.split(',');
+			$scope.books[i].numberOfIssues = list.length;
+		}
+	});	
 });
