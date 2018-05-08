05cedce18719b9c0b28c04430e9df24a57547261
Brennan Zuber <geekssw@yahoo.com>
2018-05-08 13:39:10 -0700

    quick fix

--- api/database.js
+++ api/database.js
@@ -9,7 +9,8 @@ router.use('/userEvents', require(__dirname + '/userEventsCrud.js'));
 router.use('/issue', require(__dirname + '/issueCrud.js'));
 router.use('/issueCharacter', require(__dirname + '/issueCharactersCrud.js'));
 router.use('/issueWriter', require(__dirname + '/issueWritersCrud.js'));
-router.use('/issueIllustrator', require(__dirname + '/issueIllustratorCrud.js'));
+router.use('/issueIllustrator', require(__dirname + '/issueIllustratorsCrud.js'));
+router.use('/comment', require(__dirname + '/commentsCrud.js'));
 
 router.post('/signedIn', function(req, res) {
 	var signedIn = true;
