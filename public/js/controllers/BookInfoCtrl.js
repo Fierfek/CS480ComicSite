9359b7dcbfbed257f35baf4fdf3cecaf8176305b
Sean <s.ritchie577@gmail.com>
2018-05-15 12:44:13 -0700

    Date display
    Initial commit, waiting on more info for the new queries and accessing the issues for each book.

--- public/js/controllers/BookInfoCtrl.js
+++ public/js/controllers/BookInfoCtrl.js
@@ -8,6 +8,17 @@ bookInfo.controller('BookInfoController', function($scope, $route, RestApiClient
 
 		$scope.book = response;
 
+		console.log($scope.book.publishDate);
+
+		$scope.year = new Date($scope.book.publishDate).getFullYear();
+		monthNum = new Date($scope.book.publishDate).getMonth();
+		const monthNames = ["January", "February", "March", "April", "May", "June",
+  		"July", "August", "September", "October", "November", "December"
+		];
+
+		$scope.month = monthNames[monthNum];
+
+
 		/*$scope.list = $scope.book.issueList.split(',');
 		
 		for (var i = 0; i < $scope.list.length; i++) {
@@ -18,5 +29,10 @@ bookInfo.controller('BookInfoController', function($scope, $route, RestApiClient
 		
 		//$scope.numIssues = $scope.list.length;
 		
+	});
+	RestApiClientService.get("/api/query/issue/byBook/:bookId" + $route.current.params.bookID).then(function(response){
+
+		$scope.issue = response;
+		//console.log(response);
 	});
 });
