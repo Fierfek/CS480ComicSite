fd392c9d0802eaeb0a77cb39463209c66d49f9e6
Unknown <marisabelchang@gmail.com>
2018-05-15 18:41:56 -0700

    add the spoiler button

--- public/js/controllers/IssueCtrl.js
+++ public/js/controllers/IssueCtrl.js
@@ -3,6 +3,7 @@ var issuePage = angular.module('IssueCtrl',[]);
 issuePage.controller('IssueController',function($scope,$route, RestApiClientService){
 	console.log('/issue/' + $route.current.params.issueID);
 	
+	$scope.rate = 5;
 	RestApiClientService.get('/issue/'+$route.current.params.issueID).then(function(response){
 		$scope.issue=response;
 		console.log(response);
@@ -14,19 +15,36 @@ issuePage.controller('IssueController',function($scope,$route, RestApiClientServ
 	
 	RestApiClientService.get('/query/issueCharacters/byIssue/'+$route.current.params.issueID).then(function(response){
 		$scope.characters=response;
+		console.log(response);
 	});
 	
 	RestApiClientService.get('/query/issueIllustrators/byIssue/'+$route.current.params.issueID).then(function(response){
 		$scope.illustrators=response;
 	});
 	
-	/*RestApiClientService.get('/comment/'+$route.current.params.issueID).then(function(response){
+	RestApiClientService.get('/comment/'+$route.current.params.issueID).then(function(response){
 		$scope.comments=response;
-	});*/
+	});
 	
 	var user;
 	$scope.isUser=function (user){
 		return this.user==user;
 	};
+	
+	var show=false;
+	$scope.label="more";
+	
+	$scope.isHidden = function(){
+		if(show){
+			show=false;
+			$scope.label="more";
+		}else{
+			show=true;
+			$scope.label="less";
+			
+		}
+	};
 });
+
+
 
