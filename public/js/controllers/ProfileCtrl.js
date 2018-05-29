2dd0eb69f93a1389d88eeb05e2f518200764b50a
Unknown <marisabelchang@gmail.com>
2018-05-28 21:32:03 -0700

    advanced Setting

--- public/js/controllers/ProfileCtrl.js
+++ public/js/controllers/ProfileCtrl.js
@@ -1,11 +1,12 @@
 var profile = angular.module('ProfileCtrl', []);
 
 	
-profile.controller('ProfileController', function($scope,$rootScope, $route, RestApiClientService) {
+profile.controller('ProfileController', function($scope,$rootScope, $route, RestApiClientService, PersistanceService) {
 	
 	$scope.userId=$route.current.params.userId;
 	$scope.showEdit = false;
 	$scope.editMode = false;
+	
 	
 	RestApiClientService.get("/userFavorites/" +$scope.userId ).then(function(response){
 		$scope.user = response;
@@ -27,6 +28,17 @@ profile.controller('ProfileController', function($scope,$rootScope, $route, Rest
 	
 	var exitEdit = function() {
 		$scope.editMode = false;
+	}
+	
+	$scope.isMyprofile=function(){
+		var isUser=false;
+		if($rootScope.loggedIn){
+			$scope.cookieUserId=PersistanceService.getCookieData().user;
+			if ($scope.cookieUserId == $scope.userId){
+				isUser=true;
+			}
+		}
+		return isUser;
 	}
 	
 	/*$scope.getImage = function () {
