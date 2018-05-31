7fcbb514392f96328d4cae9a08fc92fbe638b5fc
Unknown <marisabelchang@gmail.com>
2018-05-30 22:18:12 -0700

    a little change in the search bar

--- public/js/controllers/NavBarCtrl.js
+++ public/js/controllers/NavBarCtrl.js
@@ -3,12 +3,11 @@ var navBar = angular.module('NavBarCtrl', []);
 navBar.controller('NavBarController', function($scope, $rootScope, $location, PersistanceService, RestApiClientService) {
 
 	$scope.data= ["Book Title","Writer","Illustrator","Character","Issue Title","Issue by Year"];//"Book by Year"
-	
+	$scope.categoryMode=true;
+	$scope.search="chose a category";
 	$rootScope.results = {};
 	var key = PersistanceService.getCookieData();
-
 	$scope.category = "Category";
-	$scope.searchCategory=false;
 	
 	if(key) {
 		$rootScope.key = key;
@@ -25,7 +24,8 @@ navBar.controller('NavBarController', function($scope, $rootScope, $location, Pe
 	
 	$scope.setCategory = function(data) {
 		$scope.category = data;
-		$scope.searchCategory=true;
+		$scope.categoryMode=false;
+		$scope.search="";
 	}
 	
 	$scope.logOut = function () {
